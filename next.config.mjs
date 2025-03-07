b6611325ba339dec82350c14963338e1683a5c68
Sharma Aditya <aditya.sharma4231.in@gmail.com>
2025-03-07 20:38:08 +0530

    Add another dependencies for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,16 @@
-import {withSentryConfig} from '@sentry/nextjs';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
+
 
 // Suppresses source map uploading logs during build
 silent: true,
-org: "javascript-mastery",
+org: "Parva shukla",
 project: "javascript-nextjs",
 }, {
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
 
 // Upload a larger set of source maps for prettier stack traces (increases build time)
 widenClientFileUpload: true,
@@ -32,9 +30,6 @@ hideSourceMaps: true,
 // Automatically tree-shake Sentry logger statements to reduce bundle size
 disableLogger: true,
 
-// Enables automatic instrumentation of Vercel Cron Monitors.
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
+
 automaticVercelMonitors: true,
 });
